df938b3eede5620f3aa00a0e46be79f9f7394da4
matthewjmckinley <matthewjamesmckinley@gmail.com>
2026-03-18 15:46:15 -0700

    Apply and re-apply aria labels to expand toggles for communities and collections

--- asset/js/dspace-connector.js
+++ asset/js/dspace-connector.js
@@ -6,6 +6,13 @@
         $('#collapse-all').on('click', function() {
             $('.communities .collapse').click();
         });
+
+        $(document).on('o:collapsed o:expanded', '.communities a[data-name]', function() {
+            var toggle = $(this);
+            var name = toggle.data('name');
+            var action = toggle.attr('aria-label');
+            toggle.attr('aria-label', action + ' ' + name);
+        });
     });
 
     $(document).ready(function() {
