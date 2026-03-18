5cb61d809d16e118e3cb5f68cf8d4b3b3817a6e8
matthewjmckinley <matthewjamesmckinley@gmail.com>
2026-03-18 15:26:02 -0700

    Add id attribute to file ingest checkbox

--- src/Form/ImportForm.php
+++ src/Form/ImportForm.php
@@ -36,6 +36,9 @@ class ImportForm extends Form
                 'label' => 'Import files into Omeka S', // @translate
                 'info' => 'If checked, original files will be imported into Omeka S. Otherwise, derivates will be displayed when possible, with links back to the original file in the DSpace repository.', // @translate
             ],
+            'attributes' => [
+                'id' => 'ingest-files',
+            ],
         ]);
 
         // Get resource templates
